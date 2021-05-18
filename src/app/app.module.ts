d22d5714f24c5baeb5a295829bb5ac928813b666
dani ardiles <danny_ipince@hotmail.com>
2021-05-18 11:31:30 -0400

    refactor(angular): use custom prefix scotia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DishComponent } from './dish/dish.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
